c296b305db72d7b6da0dd5bdcdfd9295a7936cc0
rwern002 <rwern002@ucr.edu>
2019-04-23 08:37:45 -0700

    Added test for detecting mouse hover

---
+++ ClownsVsRobots/Assets/Scripts/GridTest.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject sObj;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit))
+        {
+            Debug.Log("Mouse is over: " + hit.collider.name);
+            GameObject hitObj = hit.transform.root.gameObject;
+            hoverObject(hitObj);
+        }
+
+        else
+        {
+            noSelect();
+        }
+    }
+
+    void hoverObject(GameObject obj)
+    {
+        if (sObj != null)
+        {
+            if (obj = sObj)
+            {
+                return;
+            }
+
+            noSelect();
+        }
+
+        sObj = obj;
+        Renderer[] rs = sObj.GetComponentsInChildren<Renderer>();
+        foreach (Renderer r in rs)
+        {
+            Material m = r.material;
+            m.color = Color.green;
+            r.material = m;
+        }
+    }
+
+    void noSelect()
+    {
+        sObj = null;
+    }
+}
