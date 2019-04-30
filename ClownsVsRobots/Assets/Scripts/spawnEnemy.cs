49bbcaaf8672817b0d43b1c4f46f54e82b02ad75
wspalding <thisisspalding@gmail.com>
2019-04-29 19:04:59 -0700

    demo 4/30

--- ClownsVsRobots/Assets/Scripts/spawnEnemy.cs
+++ ClownsVsRobots/Assets/Scripts/spawnEnemy.cs
@@ -6,21 +6,23 @@ public class spawnEnemy : MonoBehaviour
 {
 	private int spawn_timer;
 	public int spawn_time;
+	public GameObject enemy_prefab;
     // Start is called before the first frame update
     void Start()
     {
     	spawn_timer = 0;
-        spawn_time = 50;
+        spawn_time = 500;
     }
 
     // Update is called once per frame
     void Update()
     {
     	spawn_timer++;
-    	if(spawn_timer == spawn_time)
+    	Debug.Log(spawn_timer);
+    	if(spawn_timer >= spawn_time)
     	{
     		//spawn a new enemy
-    		// enemy has scripts: EnemyMovement, EnemyHealth, EnemyAttack
+    		Instantiate(enemy_prefab, new Vector3(189, -30, -65), Quaternion.identity);
     		spawn_timer = 0;
     	}
     }
